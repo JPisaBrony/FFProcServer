7b0d121507420feeff8ac9b4b99d1e5c90b9b872
JPisaBrony <jdpohl789@gmail.com>
2019-04-10 22:24:12 -0500

    + base code

---
+++ ffserver.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from subprocess import Popen, PIPE
+import uuid
+import os
+import json
+
+app = Flask("ffserver", static_url_path='')
+processing = False
+
+@app.route("/")
+def root():
+    return app.send_static_file("index.html")
+
+@app.route("/ffmpeg", methods=['POST'])
+def ffmpeg():
+    global processing
+    if processing == True:
+        return jsonify({ "result": "processing..." })
+    processing = True
+
+    vidID = str(uuid.uuid4())
+    outDir = "static/" + vidID
+    os.makedirs(outDir)
+    cmd = request.json["cmd"].replace("ffmpeg ", "").replace("\"", "")
+    cmdArgs = ["ffmpeg"]
+    for c in cmd.split(" "):
+        cmdArgs.append(c)
+
+    proc = Popen(cmdArgs, cwd=outDir, stdout=PIPE)
+    stdout, stderr = proc.communicate()
+
+    result = proc.wait()
+    processing = False
+    if result == 1:
+        os.rmdir(outDir)
+        return jsonify({"error": "some error occured"})
+    return jsonify({ "result": vidID + "/" + cmdArgs[-1] })
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
